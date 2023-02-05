fee89aa5f7dd60deeda34fe45ef70b31908e9633
nicolasey <nicolas.sey@gmail.com>
2023-02-05 17:20:01 +0100

    add postcss config and structure

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
+    "postcss-import": {},
+    "postcss-preset-env": {
+      stage: 1,
+    },
+    "postcss-simple-vars": {},
+    "postcss-nested": {},
+    "postcss-mixins": {},
     tailwindcss: {},
     autoprefixer: {},
-  },
+    cssnano: {}
+  }
 }
