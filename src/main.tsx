4c5d2646a4862a5f85d6beddab055d215c12e5ae
nicolasey <nicolas.sey@gmail.com>
2023-02-13 07:09:10 +0100

    wip Spinner

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import "./assets/css/index.min.css"
+import { Spinner } from './ui/Spinner'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback={<Spinner />}>
+      <App />
+    </React.Suspense>
   </React.StrictMode>,
 )
